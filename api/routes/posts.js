11a898e0274e834ca0864647fec140fc1c7aa74a
Chirag <chirag.1@iitj.ac.in>
2022-02-06 01:52:16 +0530

    Delete a post

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { isValidObjectId } = require("mongoose");
 const router = express.Router();
 const Post = require("../db/models/Post");
 
@@ -16,5 +17,28 @@ router.get("/", async (req, res) => {
     console.error(err);
   }
 });
+
+router.patch(':/id', async(req, res) => {
+  try{
+    const {isdeleted, deleted} = req.body
+    const {postId} = req.params
+
+    if(!isValidObjectId(postId))
+      return res.status(401).json({error: "Invalid request!"})
+    
+    const post = await Post.findById(req.params.id)
+
+    if(!post)
+      return res.status(404).json({error: "Post not found!"})
+
+    post.isdeleted = isdeleted
+    post.deleted = deleted
+
+    await post.save()
+    
+  }catch(err){
+    console.error(err)
+  }
+})
 
 module.exports = router;
