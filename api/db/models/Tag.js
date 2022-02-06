8c2add81b669c99d608e566f57a2d7a9b5f6fd2a
Chirag <chirag.1@iitj.ac.in>
2022-02-06 12:48:31 +0530

    tag_schema

--- api/db/models/Tag.js
+++ api/db/models/Tag.js
@@ -1,27 +1,23 @@
 const mongoose = require('mongoose')
 
-const tagSchema = mongoose.Schema({
-    tagId : {
-        type : String,
-        unique : true,
-        required : true,
-        trim : true
+const tagSchema = mongoose.Schema(
+    {
+        name : {
+            type : String,
+            required : true,
+            trim : true
+        },
+        desc : {
+            type : String,
+            trim : true
+        },
+        type : {
+            type : String,
+            required : true,
+            trim : true
+        }
     },
-    name : {
-        type : String,
-        required : true,
-        trim : true
-    },
-    desc : {
-        type : String,
-        trim : true
-    },
-    type : {
-        type : String,
-        required : true,
-        trim : true
-    }
-
-})
+    { timestamps: true }
+)
 
 module.exports = mongoose.model('tag', tagSchema)
