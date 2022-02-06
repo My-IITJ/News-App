3283ee85462b86bf9aa62ae8d8d261ade7a4cfb3
Sakshi Jain <jain.60@iitj.ac.in>
2022-02-06 19:32:45 +0530

    Comment Schema Created

---
+++ api/db/models/Comment.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+const commentSchema = new mongoose.Schema({
+    author:{
+        type : mongoose.Schema.Types.ObjectId,
+        required : true,
+        ref : "User",
+    },
+    content :{
+        type : String,
+        required : true,
+    },
+    replies :[
+        {
+            reply :{
+                type : mongoose.Schema.Types.ObjectId,
+                ref : "Comment",
+            }
+        }
+    ],
+    upvotes :{
+        type : Number,
+        required : true,
+    },
+    isDeleted : {
+        type : Boolean,
+        required: true,
+    },
+    deleted : {
+        at : {
+            type : Date,
+        },
+        by : {
+            type : mongoose.Schema.Types.ObjectId,
+            ref : "User"
+        }
+    }
+},{
+    timestamps : true,
+});
+
+module.exports = mongoose.model('Comment', commentSchema)
