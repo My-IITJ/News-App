f53f38adff060bbc9e7de1c570620daeadca1676
Azad Kshitij <azadkshitij08302001@gmail.com>
2022-05-10 22:55:04 +0530

    Image picker Done on new post

--- client/apiCalls/client.js
+++ client/apiCalls/client.js
@@ -1,6 +1,8 @@
 // when calling api's in react native...we cannot use localhost
 // so use your local ip address
-export const appUrl = 'http://172.30.13.123:8080/api'; //'https://myiitj-api.vercel.app/api';
-export const authUrl = 'http://172.30.13.123:8080/auth'; //'https://myiitj-api.vercel.app/auth';
+export const appUrl = 'https://myiitj-api.vercel.app/api';
+export const authUrl =  'https://myiitj-api.vercel.app/auth';
+// export const appUrl = //'http://172.30.13.123:8080/api'; 'https://myiitj-api.vercel.app/api';
+// export const authUrl = //'http://172.30.13.123:8080/auth'; //'https://myiitj-api.vercel.app/auth';
 export const defaultImgUrl =
 	'https://res.cloudinary.com/myiitj/image/upload/v1652015359/icon_qhwzl2.png';
