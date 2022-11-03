6e73fcfebf913c03bcac2a2b0e2e0fa0c62a50b3
Sandeep Swami <swami.2@iitj.ac.in>
2022-11-03 18:52:29 +0530

    Added Sections on Drawer

---
+++ client/components/Acad.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { Text, View, StyleSheet, Linking } from "react-native";
+import { Card, Button, Title, Paragraph } from "react-native-paper";
+
+const Acad = () => {
+	return (
+		<View>
+			<Card style={Styles.container}>
+				<Card.Content>
+					<Title>ERP Portal</Title>
+				</Card.Content>
+				<Card.Cover source={require("../assets/images/ERP_Portal.png")} />
+				<Card.Content>
+					<Paragraph>Welcome to new academic portal of IIT Jodhpur</Paragraph>
+				</Card.Content>
+				<Card.Actions>
+					<Button
+						onPress={() => {
+							Linking.openURL("http://220.158.144.41:8080/ERP_IITJ/");
+						}}
+					>
+						Visit
+					</Button>
+				</Card.Actions>
+			</Card>
+			<Card style={Styles.container}>
+				<Card.Content>
+					<Title>Library</Title>
+				</Card.Content>
+				<Card.Cover source={require("../assets/images/Library.png")} />
+				<Card.Content>
+					<Paragraph>
+						Explore the world’s knowledge : IIT Jodhpur Library
+					</Paragraph>
+				</Card.Content>
+				<Card.Actions>
+					<Button
+						onPress={() => {
+							Linking.openURL("https://library.iitj.ac.in/");
+						}}
+					>
+						Visit
+					</Button>
+				</Card.Actions>
+			</Card>
+		</View>
+	);
+};
+export default Acad;
+
+const Styles = StyleSheet.create({
+	container: {
+		alignContent: "center",
+		margin: 37,
+	},
+});
--- client/screens/Academics.jsx
+++ client/screens/Academics.jsx
@@ -1,35 +1,16 @@
-import { Text, Linking, Image, TouchableOpacity } from 'react-native';
-import React from 'react';
-import styled from 'styled-components/native';
-import Constants from 'expo-constants';
-import { COLORS } from '../constants';
-import Animated from 'react-native-reanimated';
-import NavBar from '../components/Navbar';
+import styled from "styled-components/native";
+import Constants from "expo-constants";
+import { COLORS } from "../constants";
+import { Image, Text } from "react-native";
+import Animated from "react-native-reanimated";
+import NavBar from "../components/Navbar";
+import Acad from "../components/Acad";
 
 const Academics = ({ drawerAnimatedStyle }) => {
 	return (
 		<Container style={[drawerAnimatedStyle]}>
-			<NavBar/>
-			<Image
-				source={require('../assets/images/coming_soon.png')}
-				style={{ width: 250, height: 250, borderRadius: 10 }}
-			/>
-			<TouchableOpacity
-				style={{
-					marginVertical: 20,
-					border: '2px solid black',
-					borderRadius: 10,
-					padding: 15,
-					backgroundColor: COLORS.white2,
-				}}
-				onPress={() => {
-					Linking.openURL(
-						'https://iitj.ac.in/department/index.php?id=departments'
-					);
-				}}
-			>
-				<Text style={{ fontSize: 30 }}>Academics</Text>
-			</TouchableOpacity>
+			<NavBar />
+			<Acad />
 		</Container>
 	);
 };
@@ -39,9 +20,7 @@ export default Academics;
 //styles
 const Container = styled(Animated.View)`
 	flex: 1;
-	justify-content: center;
-	align-items: center;
 	background-color: ${({ theme }) =>
-		theme.name === 'dark' ? COLORS.darkPurple : COLORS.white1};
+		theme.name === "dark" ? COLORS.darkPurple : COLORS.white1};
 	padding-top: ${Constants.statusBarHeight}px;
 `;
