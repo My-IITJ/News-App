1ccffc54c0ffcb83e9ed05c5ba2c58e355074924
samarth-1729 <vijay.5@iitj.ac.in>
2022-11-09 20:31:28 +0530

    utilities added

--- client/screens/Utilities.jsx
+++ client/screens/Utilities.jsx
@@ -9,7 +9,8 @@ import UtilityCard from "../components/UtilityCard";
 const utilities = [
   {
     title: "Socials",
-    image: "",
+    image:
+      "https://res.cloudinary.com/myiitj/image/upload/v1667922690/3_gr5qd2.jpg",
     desc: "Follow us on social media to get latest updates.",
     options: {
       title: "Links",
@@ -52,12 +53,12 @@ const utilities = [
         {
           type: "call",
           name: "Reception",
-          value: "12345678",
+          value: "+91 291 280 1190",
         },
         {
           type: "call",
           name: "Ambulance",
-          value: "12345678",
+          value: "+91 75688 15630",
         },
       ],
     },
@@ -79,7 +80,80 @@ const utilities = [
         {
           type: "call",
           name: "E Rickshaw",
+          value: "8003382240",
+        },
+        {
+          type: "call",
+          name: "Bus",
           value: "12345678",
+        },
+      ],
+    },
+  },
+  {
+    title: "Kendriya Bhandar",
+    image:
+      "https://res.cloudinary.com/myiitj/image/upload/v1668003024/berm_aod8xi.jpg",
+    desc: "Kendriya Bhandar store has a variety of grocery items from stationary to snacks.",
+    options: {
+      title: "Details",
+      items: [
+        {
+          type: "call",
+          name: "Contact",
+          value: "6375723425",
+        },
+        {
+          type: "location",
+          name: "Near type-B housing",
+          value: "",
+        },
+      ],
+    },
+  },
+  {
+    title: "Grocery/Bakery shop",
+    image:
+      "https://res.cloudinary.com/myiitj/image/upload/v1668003024/berm_aod8xi.jpg",
+    desc: "A mini supermarket for all your daily needs as well as fresh baked products from 15 A.D",
+    options: {
+      title: "Details",
+      items: [
+        {
+          type: "call",
+          name: "Contact",
+          value: "12345678",
+        },
+        {
+          type: "location",
+          name: "W-21 Berm",
+          value: "",
+        },
+      ],
+    },
+  },
+  {
+    title: "Stationary",
+    image:
+      "https://res.cloudinary.com/myiitj/image/upload/v1668003024/berm_aod8xi.jpg",
+    desc: "You can get basic stationary and photocopies from here.",
+    options: {
+      title: "Details",
+      items: [
+        {
+          type: "call",
+          name: "Contact 1",
+          value: "7878592910",
+        },
+        {
+          type: "call",
+          name: "Contact 2",
+          value: "9785887445",
+        },
+        {
+          type: "location",
+          name: "E-21 Berm",
+          value: "",
         },
       ],
     },
@@ -97,6 +171,7 @@ const utilities = [
           name: "Addie's Bakery",
           value: "12345678",
         },
+        // all other contacts
       ],
     },
   },
