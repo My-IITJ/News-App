0ff1dac59d8bc0fe7e1f70862d1a465de29c0a17
Pratyaksh <tyagipratyaksh@gmail.com>
2022-11-10 18:02:01 +0530

    Minor Fix

--- client/apiCalls/auth.js
+++ client/apiCalls/auth.js
@@ -3,11 +3,7 @@ import { BASE_URL_AUTH } from './client';
 
 // get user details with jwt token
 export const updateUserData = async (data) => {
-	const res = await axios.post(`${BASE_URL_AUTH}/user-details`, data, {
-		headers: {
-			'Content-Type': 'application/json',
-		},
-	});
+	const res = await axios.post(`${BASE_URL_AUTH}/user-details`, data);
 
 	return res.data;
 };
--- client/apiCalls/client.js
+++ client/apiCalls/client.js
@@ -1,9 +1,9 @@
 // when calling api's in react native...we cannot use localhost
 // so use your local ip address
-const BASE_URL_DEV = 'http://192.168.0.102:8080/api';
-const BASE_URL_PROD = 'https://api-my-iitj.vercel.app/api';
-export const BASE_URL_AUTH = 'http://192.168.0.102:8080/auth'
-export const appUrl = BASE_URL_DEV; //'http://172.30.13.123:8080/api';
+const BASE_URL_DEV = 'http://192.168.0.102:8080';
+const BASE_URL_PROD = 'https://api-my-iitj.vercel.app';
+export const BASE_URL_AUTH = BASE_URL_PROD + '/auth'
+export const appUrl = BASE_URL_PROD + "/api"; //'http://172.30.13.123:8080/api';
 export const defaultImgUrl =
 	'https://res.cloudinary.com/myiitj/image/upload/v1652015359/icon_qhwzl2.png';
 
