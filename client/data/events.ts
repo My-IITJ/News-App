436813947819e4ceef939733bb8aaa78d02fb7c5
Shivam1007 <sharma.56@iitj.ac.in>
2022-11-11 14:01:38 +0530

    Added Photos

--- client/data/events.ts
+++ client/data/events.ts
@@ -22,27 +22,59 @@ export const fests = [
       "https://images.unsplash.com/photo-1429823040067-2c31b1d637ae?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
     original:
       "https://images.unsplash.com/photo-1429823040067-2c31b1d637ae?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2734&q=80",
-    images: [],
+    images: [
+      "https://res.cloudinary.com/myiitj/image/upload/v1667997477/Events/Aaftaab%2722/IMG_1111_slam6r.jpg",
+      "https://res.cloudinary.com/myiitj/image/upload/v1667997454/Events/Aaftaab%2722/IMG_1065_xn23ce.jpg",
+      "https://res.cloudinary.com/myiitj/image/upload/v1667997450/Events/Aaftaab%2722/IMG_1414_icnbgy.jpg",
+      "https://res.cloudinary.com/myiitj/image/upload/v1667997375/Events/Aaftaab%2722/IMG_1111_vt3gqj.jpg",
+    ],
     videos: [],
   },
   {
-    name: "Prometeo '22",
-    desc: "Tech Festival",
+    name: "Youth Conclave '22",
+    desc: "Emerging Technologies for Sustainable Future",
     thumbnail:
       "https://images.unsplash.com/photo-1480374178950-b2c449be122e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
     original:
       "https://images.unsplash.com/photo-1480374178950-b2c449be122e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80",
-    images: [],
+    images: [
+      "https://res.cloudinary.com/myiitj/image/upload/v1668065319/Events/Youth%20Conclave%2022/IMG_7741_wm5fyh.jpg",
+      "https://res.cloudinary.com/myiitj/image/upload/v1668065319/Events/Youth%20Conclave%2022/IMG_7964_jrqyja.jpg",
+      "https://res.cloudinary.com/myiitj/image/upload/v1668065317/Events/Youth%20Conclave%2022/IMG_6857_grmuo5.jpg",
+      "https://res.cloudinary.com/myiitj/image/upload/v1668065317/Events/Youth%20Conclave%2022/IMG_8056_xcwx6u.jpg",
+      "https://res.cloudinary.com/myiitj/image/upload/v1668065317/Events/Youth%20Conclave%2022/IMG_6830_slg7xh.jpg",
+      "https://res.cloudinary.com/myiitj/image/upload/v1668065315/Events/Youth%20Conclave%2022/IMG_7793_k3bqbs.jpg",
+      "https://res.cloudinary.com/myiitj/image/upload/v1668065309/Events/Youth%20Conclave%2022/IMG_6144_nebww0.jpg",
+      "https://res.cloudinary.com/myiitj/image/upload/v1668065308/Events/Youth%20Conclave%2022/IMG_6485_r7csdo.jpg",
+      "https://res.cloudinary.com/myiitj/image/upload/v1668065303/Events/Youth%20Conclave%2022/IMG_0004_qwce1z.jpg",
+      "https://res.cloudinary.com/myiitj/image/upload/v1668065301/Events/Youth%20Conclave%2022/IMG_0008_e1wyth.jpg",
+      "https://res.cloudinary.com/myiitj/image/upload/v1668065297/Events/Youth%20Conclave%2022/IMG_7450_kley7f.jpg",
+      "https://res.cloudinary.com/myiitj/image/upload/v1668065295/Events/Youth%20Conclave%2022/IMG_6540_ptji6i.jpg",
+    ],
     videos: [],
   },
   {
-    name: "Ignus '22",
-    desc: "Overall Biggest Festival",
+    name: "Diwali '22",
+    desc: "The Festival of Lights",
     thumbnail:
       "https://images.unsplash.com/photo-1523540383849-4ae4125acd6f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
     original:
       "https://images.unsplash.com/photo-1523540383849-4ae4125acd6f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80",
-    images: [],
+    images: [
+      "https://res.cloudinary.com/myiitj/image/upload/v1668064125/Events/Diwali%2022/IMG20221024225226_-_Alok_Kumar_B21CS006_an5die.jpg",
+      "https://res.cloudinary.com/myiitj/image/upload/v1668064124/Events/Diwali%2022/PicsArt_10-25-10.56.51_-_Manoj_Kumar_Ghosh_M21PH015_m3ihk9.jpg",
+      "https://res.cloudinary.com/myiitj/image/upload/v1668064124/Events/Diwali%2022/IMG20221024205443_-_Ravikant_P22PH007_ufmfvh.jpg",
+      "https://res.cloudinary.com/myiitj/image/upload/v1668064120/Events/Diwali%2022/IMG20221024214430_-_Alok_Kumar_B21CS006_bdncpu.jpg",
+      "https://res.cloudinary.com/myiitj/image/upload/v1668064120/Events/Diwali%2022/IMG20221024214658_-_Alok_Kumar_B21CS006_fqg6jd.jpg",
+      "https://res.cloudinary.com/myiitj/image/upload/v1668064116/Events/Diwali%2022/IMG_0783_-_Satish_Kumar_Adari_P20CI202_jcijnk.heic",
+      "https://res.cloudinary.com/myiitj/image/upload/v1668063988/Events/Diwali%2022/Image_1_-_Raj_Bhushan_Bara_M21CY020_snlyyw.jpg",
+      "https://res.cloudinary.com/myiitj/image/upload/v1668063988/Events/Diwali%2022/2_-_Abhishek_P20MT001_yrj8hx.jpg",
+      "https://res.cloudinary.com/myiitj/image/upload/v1668063985/Events/Diwali%2022/Copy_of_1_-_Abhishek_P20MT001_w0ypt5.jpg",
+      "https://res.cloudinary.com/myiitj/image/upload/v1668063983/Events/Diwali%2022/IMG_20221023_222952-01_-_Soham_Padhye_M22RM007_gfbalm.jpg",
+      "https://res.cloudinary.com/myiitj/image/upload/v1668063978/Events/Diwali%2022/IMG_20221024_201748__01_-_Piyush_B21BB022_l3k262.jpg",
+      "https://res.cloudinary.com/myiitj/image/upload/v1668063977/Events/Diwali%2022/IMG_20221024_230335_-_Manoj_Kumar_Ghosh_M21PH015_gj3m2b.jpg",
+
+    ],
     videos: [],
   },
 ];
